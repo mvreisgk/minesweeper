57edd8a5fbe60d2c9c5a3219295b6656cd5966bf
Marcus Vinicius <marcus.vinicius@kalyst.com.br>
2023-07-09 20:49:01 -0300

    prettier eslint

--- src/app/components/game/Header.tsx
+++ src/app/components/game/Header.tsx
@@ -1,5 +1,11 @@
 import { FC } from 'react'
 
-const Header: FC = () => <header>fseger</header>
+const Header: FC = () => (
+  <header className="flex h-8 w-full flex-row justify-evenly bg-gray-500">
+    <div>1</div>
+    <div>2</div>
+    <div>3</div>
+  </header>
+)
 
 export default Header
--- src/app/components/game/Wrapper.tsx
+++ src/app/components/game/Wrapper.tsx
@@ -3,7 +3,7 @@ import Header from './Header'
 
 const Wrapper: FC = () => (
   <main className="flex h-screen w-screen items-center justify-center bg-black p-2">
-    <section className="flex h-full w-3/4 items-center justify-center bg-sky-400">
+    <section className="flex aspect-square h-full flex-col items-center justify-start bg-sky-400">
       <Header />
     </section>
   </main>
